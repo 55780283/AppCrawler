e94096d2a89fdd2bdab478b549763afaaa5ec868
liumingwei <55780283@qq.com>
2020-10-19 11:20:30 +0800

    更新

--- AppCrawler/run.sh
+++ AppCrawler/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-java -jar AppCrawler/appcrawler-2.4.0.jar -c AppCrawler/MyConfig.yml
+#java -jar AppCrawler/appcrawler-2.4.0.jar -c AppCrawler/MyConfig.yml
+
+killAppium(){
+    ps -ax | grep 4723 | awk '{print $1}' | xargs kill -9
+}
+
+startAppium(){
+    appium --session-override  -p  4723 &
+}
+
+runAppCrawler(){
+    java -jar appcrawler-2.4.0.jar -c MyConfig.yml
+}
